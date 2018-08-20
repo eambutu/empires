0ec5b0289801cb5f07d6396b47cfb468f7a3f2bc
Abhinav Venigalla <abhinav.venigalla@gmail.com>
2018-08-19 20:46:07 -0700

    add initial ws

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 var express = require('express');
 var app = express();
+var expressWs = require('express-ws')(app);
+
 app.get('/', function (req, res) {
     res.send('Hello World!');
 });
+
+var wss = expressWs.getWss('/');
+
+app.ws('/', function (ws, req) {
+    ws.on('message', function (msg) {
+        console.log(`Received ${msg}`);
+        ws.send(`Hello, you sent -> ${msg}`);
+    });
+
+    if (wss.clients.size <= 2) {
+        ws.send(`You are Player ${wss.clients.size}`);
+        console.log('size is', wss.clients.size);
+    }
+    else {
+        ws.send(`Lobby is full`)
+    }
+});
+
 app.listen(5000, function () {
-    console.log('App listening on port 3000!');
+    console.log('App listening on port 5000!');
 });
 
 // create a GET route
 app.get('/express_backend', (req, res) => {
-    res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
+    res.send({express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT'});
 });
