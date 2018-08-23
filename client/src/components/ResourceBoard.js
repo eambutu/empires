fbe33c12adfbd6741cbe15c671093a9806c77b61
Phillip <kip.phil@gmail.com>
2018-08-23 14:45:24 -0700

    Flash red when insufficient shards

--- client/src/components/ResourceBoard.js
+++ client/src/components/ResourceBoard.js
@@ -3,17 +3,17 @@ import '../styles/ResourceBoard.css';
 import shard from '../shard_icon.svg'
 
 export default function ResourceBoard(props) {
-    const {displayShards} = props;
+    const {displayShards, insufficientShards} = props;
 
     return (
         <div className={"resource-info"}>
-            <div className={"shard-count"}>
+            <div className={insufficientShards ? "shard-count shard-red" : "shard-count"}>
                 <span>
                     <object className={"shards-icon"} type={"image/svg+xml"} data={shard}>
                         Your browser does not support SVG
                     </object>
                     {displayShards}
-                    </span>
+                </span>
             </div>
         </div>
     )
