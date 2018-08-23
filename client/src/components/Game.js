1d85cc42fbed4af5d57c5660869d6a62f0a7489e
Abhinav Venigalla <abhinav.venigalla@gmail.com>
2018-08-23 15:00:16 -0700

    fix keybound crash

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -208,9 +208,6 @@ class Game extends Component {
     }
 
     componentDidMount() {
-        document.addEventListener("keydown", this.keyDownBound);
-        document.addEventListener("keyup", this.keyUpBound);
-
         this.ws = new WebSocket('ws://' + window.location.hostname + ':5000' + window.location.pathname);
 
         this.onReset = e => {
@@ -243,6 +240,8 @@ class Game extends Component {
                     playerIds: data.playerIds,
                     spawnSquare: data.spawn
                 });
+                document.addEventListener("keydown", this.keyDownBound);
+                document.addEventListener("keyup", this.keyUpBound);
             } else if (data.event === 'update') {
                 this.updateGame(data.state);
             } else if (data.event === 'full') {
