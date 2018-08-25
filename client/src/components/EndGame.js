3b63ff0d8101b1dd60cc970bd2741211f81a0bdf
Abhinav Venigalla <abhinav.venigalla@gmail.com>
2018-08-24 18:12:16 -0700

    remove extra blank in endgame.js

--- client/src/components/EndGame.js
+++ client/src/components/EndGame.js
@@ -7,9 +7,8 @@ export default function EndGame(props) {
         <div className={"center endgame"}>
         <h1>{status === "won" ? "Victory!" : "Defeat!"}</h1>
             {canPlayAgain &&
-                <button className={"end-button"} onClick={resetClick}>Play Again</button>
+                <button className={"end-button"} onClick={resetClick}>Play Again</button> && <br></br>
             }
-            <br></br>
             <button className={"end-button"} onClick={exitClick}>Exit</button>
         </div>
     );
