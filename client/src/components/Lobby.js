dbd6d4320cea836b3c2d5bc82cd1498f565a8532
Alex Chen <achen19@mit.edu>
2018-08-25 15:29:59 -0700

    custom lobby ui

--- client/src/components/Lobby.js
+++ client/src/components/Lobby.js
@@ -1,15 +1,36 @@
 import React from 'react';
 import '../styles/Lobby.css';
+import sword from "../sword.svg";
 
 export default function Lobby(props) {
     const {onPlayerReady, playerIds, playerStatus, waitingText} = props;
-    console.log(playerStatus)
-    console.log(playerIds)
     return (
         <div className={"center"}>
-            {waitingText}
-            {playerIds}
-            <button onClick={onPlayerReady}> hi </button>
+            <div className={"lobby-title"}>
+                <img src={sword} className="App-logo" alt="logo"/>
+                <div className="title">Lobby Name</div>
+            </div>
+            <div style={{margin:"50px"}}> Game Type: <br/> <button id={"gameTypeButton"}> Duel </button> </div>
+                {waitingText}
+                <br/>
+                <table className={"lobby-player-table"}>
+                    <tbody>
+                    <tr>
+                        <td style={{minWidth: "100px", textAlign: "left"}}>
+                            <div className={"lobby-display-name"}>
+                                Name 1
+                            </div>
+                        </td>
+                        <td style={{minWidth: "100px", textAlign: "right"}}>
+                            <button className="ready-up-button" onClick={onPlayerReady}>
+                                <div>ready up!
+                                </div>
+                            </button>
+                        </td>
+
+                    </tr>
+                    </tbody>
+                </table>
         </div>
     )
 
