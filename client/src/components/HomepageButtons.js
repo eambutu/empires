09c86eb0d94019ac466cb82524940605b55865a5
Alex Chen <achen19@mit.edu>
2018-08-24 21:34:59 -0700

    username works

--- client/src/components/HomepageButtons.js
+++ client/src/components/HomepageButtons.js
@@ -2,18 +2,20 @@ import React from "react";
 import arrow from "../arrow.svg";
 
 export default function HomepageButtons(props) {
-    const {onClickFFA, goToPlayMenu, menuIndex} = props;
+    const {onRegisterUsername, onClickFFA, goToPlayMenu, menuIndex} = props;
     console.log(menuIndex);
     switch(menuIndex) {
         case 0:
             return (
                 <div id={"homepage-buttons"}>
                     <div>
-                        <input type="text" id="room_id" placeholder="Username"/> <br/>
+                        <form onSubmit={onRegisterUsername()} action="#">
+                            <input type="text" id="username" placeholder="Username"/> <br/>
+                        </form>
                     </div>
-                    <button className="homepage-button" onClick={goToPlayMenu}>Play!</button>
+                    <button className="homepage-button" onClick={() => {onRegisterUsername(); goToPlayMenu()}}>Play!</button>
                     <br/>
-                    <button className="homepage-button" onClick={() => {window.location = "/tutorial/"}}>Tutorial</button>
+                    <button className="homepage-button" onClick={() => {onRegisterUsername(); window.location = "/tutorial/"}}>Tutorial</button>
                 </div>
             )
 
