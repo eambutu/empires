1a3e7f65a211538d9119e6b3068ffe40b5d9b425
Alex Chen <achen19@mit.edu>
2018-08-25 19:11:03 -0700

    make buttons works in lobby:

--- client/src/components/Lobby.js
+++ client/src/components/Lobby.js
@@ -1,28 +1,50 @@
 import React from 'react';
 import '../styles/Lobby.css';
 import sword from "../sword.svg";
+import {ReadyType} from "./config"
 
 export default function Lobby(props) {
-    const {statuses, togglePlayerReady, playerIds, playerStatus, waitingText, active} = props;
+    const {playerId, statuses, togglePlayerReady, playerIds, playerStatus, waitingText, active} = props;
     let roomName = window.location.href.split("/").pop()
     console.log(active)
-
+    console.log(playerId)
     console.log(statuses)
-    let rows = Object.keys(statuses).map((id, index) => (
-        <tr>
-            <td style={{minWidth: "100px", textAlign: "left"}}>
-                <div className={"lobby-display-name"}>
-                    {statuses[id]['name']}
-                </div>
-            </td>
-            <td style={{minWidth: "100px", textAlign: "right"}}>
-                <button className={statuses[id]['status'] ? "ready-up-button-active" : "ready-up-button"} onClick={togglePlayerReady}>
-                    <div>{statuses[id]['status'] ? "ready" : "ready up"}
+    let rows = Object.keys(statuses).map((id, index) => {
+        console.log(id)
+        if (id === playerId) {
+            console.log("HIIII")
+            return (
+                <tr>
+                    <td style={{minWidth: "100px", textAlign: "left"}}>
+                        <div className={"lobby-display-name"}>
+                            {statuses[id]['name']}
+                        </div>
+                    </td>
+                    <td style={{minWidth: "100px", textAlign: "right"}}>
+                        <button className={statuses[id]['ready'] === ReadyType.READY ? "ready-up-button-active" : "ready-up-button"} onClick={togglePlayerReady}>
+                            <div>{statuses[id]['ready'] === ReadyType.READY ? "ready" : "ready up"}
+                            </div>
+                        </button>
+                    </td>
+                </tr>
+            )
+        }
+        return (
+            <tr>
+                <td style={{minWidth: "100px", textAlign: "left"}}>
+                    <div className={"lobby-display-name"}>
+                        {statuses[id]['name']}
                     </div>
-                </button>
-            </td>
-        </tr>
-    ));
+                </td>
+                <td style={{minWidth: "100px", textAlign: "right"}}>
+                    <button className={'other-player-ready-status-button'}>
+                        <div>{statuses[id]['ready'] === ReadyType.READY ? "ready" : "Not yet ready"}
+                        </div>
+                    </button>
+                </td>
+            </tr>
+        )
+    });
     return (
         <div className={"custom-lobby-title"}>
             <div className={"lobby-title"}>
